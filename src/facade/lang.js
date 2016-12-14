473cbfbca0adeac3749e45943b5126f9b913e2bc
Victor Berchet <victor@suumit.com>
2016-12-14 01:40:44 +0000

    f5f1d5f fix(compiler): make sure provider values with `name` property don’t break.

--- src/facade/lang.js
+++ src/facade/lang.js
@@ -62,10 +62,10 @@ function stringify(token) {
         return '' + token;
     }
     if (token.overriddenName) {
-        return token.overriddenName;
+        return "" + token.overriddenName;
     }
     if (token.name) {
-        return token.name;
+        return "" + token.name;
     }
     var res = token.toString();
     var newLineIndex = res.indexOf('\n');
