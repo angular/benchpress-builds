f57f199973de6f2a1fa6283a2459988376d0a23a
Miško Hevery <misko@hevery.com>
2017-03-22 00:17:31 +0000

    5efc860 fix(forms): make composition event buffering configurable (#15256)

---
+++ src/validator/size_validator.d.ts
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { InjectionToken } from '@angular/core';
+import { MeasureValues } from '../measure_values';
+import { Validator } from '../validator';
+/**
+ * A validator that waits for the sample to have a certain size.
+ */
+export declare class SizeValidator extends Validator {
+    private _sampleSize;
+    static SAMPLE_SIZE: InjectionToken<{}>;
+    static PROVIDERS: (typeof SizeValidator | {
+        provide: InjectionToken<{}>;
+        useValue: number;
+    })[];
+    constructor(_sampleSize: number);
+    describe(): {
+        [key: string]: any;
+    };
+    validate(completeSample: MeasureValues[]): MeasureValues[];
+}
