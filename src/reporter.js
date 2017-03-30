d64f6471c994466412715dc565d000a717ba4591
Victor Berchet <victor@suumit.com>
2017-03-30 00:21:28 +0000

    67719f2 docs: add changelog for 4.1.0-beta.0

---
+++ src/reporter.js
@@ -0,0 +1,19 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+"use strict";
+/**
+ * A reporter reports measure values and the valid sample.
+ */
+class Reporter {
+    reportMeasureValues(values) { throw new Error('NYI'); }
+    reportSample(completeSample, validSample) {
+        throw new Error('NYI');
+    }
+}
+exports.Reporter = Reporter;
+//# sourceMappingURL=reporter.js.map
