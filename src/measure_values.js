61f0b8ab96ef7c113e2bbe3b949ab0fa8b699260
Miško Hevery <misko@hevery.com>
2017-04-21 05:23:06 +0000

    6ccb937 docs: add changelog for 4.0.3

---
+++ src/measure_values.js
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+"use strict";
+class MeasureValues {
+    constructor(runIndex, timeStamp, values) {
+        this.runIndex = runIndex;
+        this.timeStamp = timeStamp;
+        this.values = values;
+    }
+    toJson() {
+        return {
+            'timeStamp': this.timeStamp.toJSON(),
+            'runIndex': this.runIndex,
+            'values': this.values,
+        };
+    }
+}
+exports.MeasureValues = MeasureValues;
+//# sourceMappingURL=measure_values.js.map
