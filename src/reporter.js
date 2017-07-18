87b912d63924c4f67496efae7074c2387bba3541
Matias Niemelä <matias@yearofmoo.com>
2017-07-18 17:56:08 +0000

    b9bb82e fix(animations): capture cancelled animation styles within grouped animations

---
+++ src/reporter.js
@@ -0,0 +1,20 @@
+"use strict";
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * A reporter reports measure values and the valid sample.
+ */
+class Reporter {
+    reportMeasureValues(values) { throw new Error('NYI'); }
+    reportSample(completeSample, validSample) {
+        throw new Error('NYI');
+    }
+}
+exports.Reporter = Reporter;
+//# sourceMappingURL=reporter.js.map
