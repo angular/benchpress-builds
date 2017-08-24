52efb02aa974e709cf9247b306d1cb29763484e0
Misko Hevery <misko@hevery.com>
2017-08-24 20:25:02 +0000

    b94ce8a build: fix build; missing firebase-tools

---
+++ src/webdriver/ios_driver_extension.d.ts
@@ -0,0 +1,20 @@
+import { WebDriverAdapter } from '../web_driver_adapter';
+import { PerfLogEvent, PerfLogFeatures, WebDriverExtension } from '../web_driver_extension';
+export declare class IOsDriverExtension extends WebDriverExtension {
+    private _driver;
+    static PROVIDERS: {
+        provide: typeof IOsDriverExtension;
+        deps: typeof WebDriverAdapter[];
+    }[];
+    constructor(_driver: WebDriverAdapter);
+    gc(): Promise<any>;
+    timeBegin(name: string): Promise<any>;
+    timeEnd(name: string, restartName?: string | null): Promise<any>;
+    readPerfLog(): Promise<PerfLogEvent[]>;
+    /** @internal */
+    private _convertPerfRecordsToEvents(records, events?);
+    perfLogFeatures(): PerfLogFeatures;
+    supports(capabilities: {
+        [key: string]: any;
+    }): boolean;
+}
