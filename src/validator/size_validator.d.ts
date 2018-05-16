36efca07757e2d8e199f97a978bb55ff18906436
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-16 22:50:02 +0000

    95e9107 docs: add changelog for 5.2.11

---
+++ src/validator/size_validator.d.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { InjectionToken } from '@angular/core';
+import { MeasureValues } from '../measure_values';
+import { Validator } from '../validator';
+/**
+ * A validator that waits for the sample to have a certain size.
+ */
+export declare class SizeValidator extends Validator {
+    private _sampleSize;
+    static SAMPLE_SIZE: InjectionToken<{}>;
+    static PROVIDERS: ({
+        provide: typeof SizeValidator;
+        deps: InjectionToken<{}>[];
+    } | {
+        provide: InjectionToken<{}>;
+        useValue: number;
+    })[];
+    constructor(_sampleSize: number);
+    describe(): {
+        [key: string]: any;
+    };
+    validate(completeSample: MeasureValues[]): MeasureValues[] | null;
+}
